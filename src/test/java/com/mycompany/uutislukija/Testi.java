9ab2ec7b00bc59690ad2079b2ea4539c1e6d51c7
ued1 <ued1@users.noreply.github.com>
2015-09-20 13:54:15 +0300

    testi

---
+++ src/test/java/com/mycompany/uutislukija/Testi.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.uutislukija;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Testi {
+    
+    public Testi() {
+    }
+    
+    @Test
+    public void eiTestaaMitaanAinaTrue() {
+        assertTrue(true);
+    }
+    
+}
